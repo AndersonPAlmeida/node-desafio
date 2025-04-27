3f8404032176ee144026fff0efbdd9a2910091b9
Anderson Almeida <andersonpa10pa@gmail.com>
2025-04-26 22:13:38 -0300

    Primeiro commit: Desafio Node Rocketseat

---
+++ src/routes.js
@@ -0,0 +1,37 @@
+import { Database } from "./database.js"
+import { buildRoutePath } from "./utils/build-route-path.js"
+import { randomUUID } from 'node:crypto'
+
+const database = new Database()
+
+export const routes = [
+  {
+    method: 'POST',
+    path: buildRoutePath('/task'),
+    handler: (req, res) => {
+      const { name, completed } = req.body
+
+      database.insert('tasks', {
+        id: randomUUID(),
+        name,
+        completed: completed ?? false
+      })
+      
+      return res.writeHead(201).end()
+    }
+  },
+  {
+    method: 'GET',
+    path: buildRoutePath('/task'),
+    handler: (req, res) => {
+      const { search } = req.query
+      
+      const users = database.select('tasks', search ? {
+        name: search,
+        completed: search,
+      } : null)
+    
+      return res.end(JSON.stringify(users))
+    }
+  }
+]
